5f4d44f61bda33fcff23ecb3d420b37f490d9e72
adouche-js <adouche@scool.qzz.io>
2026-07-17 20:50:20 +0200

    hvcg

--- tsup.config.ts
+++ tsup.config.ts
@@ -9,8 +9,15 @@ export default defineConfig({
   sourcemap: true,
   clean: true,
   treeshake: true,
-  minify: true,
+  minify: false,
+  name: "ld",
+  target: "node18",
+  metafile: true,
+  external: ["dayjs", "msrcrypto"],
   banner: {
-    js: '// linkdirecte – universal SDK (Node.js, Bun, Deno, Browsers, Workers, React Native)',
+    js: '// © 2026 typeof (Scolup) | Licensed under AGPL 3.0',
+  },
+  footer: {
+    js: '// © 2026 typeof (Scolup) | Licensed under AGPL 3.0',
   },
 });
